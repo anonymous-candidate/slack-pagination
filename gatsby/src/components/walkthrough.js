5485e2272fbcbd5a08d4fddc2fb738a512819830
loferris <lo.k.ferris@gmail.com>
2020-06-11 18:19:27 -0700

    slack_wow: change directory names

---
+++ gatsby/src/components/walkthrough.js
@@ -0,0 +1,19 @@
+
+/*import React, { useState } from "react"
+
+const Walkthrough = props => {
+  const [query, setQuery] = useState("");
+  const onClick = (e) => {
+    setQuery(props.query);
+  }
+
+  return (
+    <div>
+      <Query query={query} />
+      <Button onClick={onClick}/>
+    </div>
+  )
+
+}
+
+export default Walkthrough;*/
